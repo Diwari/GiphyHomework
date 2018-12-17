d4c6e733d2a26e994eeb059d612a071f48bf7b4f
unknown <mrtoryan@outlook.com>
2018-12-16 19:35:38 -0500

    reverted back to old logic, looked through code with tutor and fixed error

--- assets/javascript/testJS(learn&Understand the logic).js
+++ assets/javascript/testJS(learn&Understand the logic).js
@@ -7,81 +7,92 @@ var  topic = ["Sonic", "Mario", "Megaman"];
 
 console.log( topic)
 
-function displayGifs()
+function renderButtons(topicsToUse){
 
-    var character = $(this).attr( "data-character" );
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
-    + characters + "&api_key=KNEF8r4vJQFJs9tBGSaM1VDZcIrJVyHi&limit=10"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var charDiv = $( "<div class = 'character' ");
-     
-        var results = response.data;
-        
-        var p = $( "<p>" ).text( "Rating:" + results[i].rating);
-
-        charDiv.append(p);
-
-        $( "#gifs-here").prepend(charDiv);
-
-               
-        }),
-
-
-        
- // var charImg = $("<img>");
-                // charImg.attr( "src", response.data[i].images.fixed_height_still.url);
-                // charImg.attr( "data-still", response.data[i].images.fixed_height_still.url);
-                // charImg.attr( "data-animate", response.data[i].images.original.url);
-                // charImg.attr( "data-state", "still");
-                // charImg.addClass("gif")
-
-                // charDiv.append(p);
-                // charDiv.append(charImg);
-                
-                // $("#gifs-here").prepend(charImg)
-
-    // }),$(document).on( "click", ".gif", function() {
-    //     var state = $(this).attr( "data-state" );
-    
-    //     if( state === "still" ){
-    //         $(this).attr( "src", $(this).attr( "data-animate" ));
-    //         $(this).attr( "data-state", "animate");
-    //     } else {
-    //         $(this).attr( "src", $(this).attr( "data-still" ));
-    //         $(this).attr( "data-state", "still");
-    //     }
-    
-    // })
-    
-
-
-function renderButtons(){
+    console.log('should have four?', topicsToUse)
     $("#char-button").empty();
 
-    for( var i = 0; i < topic.length; i++){
+    for( var i = 0; i < topicsToUse.length; i++){
 
        var a =$( "<button>" );
 
-        a.addClass( "char-btn" );
+        a.addClass( "character btn-primary" );
         
         a.attr( "data-character", topic[i]);
 
         a.text(topic[i]);
 
         $("#char-button").append(a);
+
     
     }
-},$( "#add-character").on( "click", function(){
+}
+$( "#addChar").on( "click", function(event){
 
     event.preventDefault();
-    var character = $("add-character").val()
+    var character = $("#add-character").val()
 
     topic.push(character);
     console.log( character)
-    renderButtons()
+    renderButtons(topic)
+}
+)
+
+$(document).on( "click", ".character", function(event){
+    event.preventDefault();
+
+    var characters = $(this).attr( "data-character" );
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
+    + characters + "&api_key=KNEF8r4vJQFJs9tBGSaM1VDZcIrJVyHi&limit=10"
+
+    $.ajax({
+
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+     var results = response.data;
+
+     var gifsDiv = $("#gifs-here");
+
+     gifsDiv.empty();
+
+     for ( var i = 0; i < results.length; i++){
+
+        var charDiv = $("<div class = 'char'>");
+      
+        var rating = results[i].rating;
+
+        var p = $( "<p>" ).text( "Rating:" + rating);
+
+        var charImg = $("<img>");
+        charImg.attr( "src", response.data[i].images.fixed_height_still.url);
+        charImg.attr( "data-still", response.data[i].images.fixed_height_still.url);
+        charImg.attr( "data-animate", response.data[i].images.original.url);
+        charImg.attr( "data-state", "still");
+        charImg.addClass("gif")
+
+        charDiv.append(p);
+        charDiv.append(charImg);
+        
+        $("#gifs-here").prepend(charImg)
+     }
+
 })
+
+
+});
+$(document).on( "click", ".gif", function() {
+        var state = $(this).attr( "data-state" );
     
+        if( state === "still" ){
+            $(this).attr( "src", $(this).attr( "data-animate" ));
+            $(this).attr( "data-state", "animate");
+        } else {
+            $(this).attr( "src", $(this).attr( "data-still" ));
+            $(this).attr( "data-state", "still");
+        }
+    
+    })
+    
+renderButtons(topic)
+
